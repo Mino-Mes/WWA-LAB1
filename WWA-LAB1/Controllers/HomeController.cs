4924584a9022caac3d3544a886acdac29c7364ae
Maestro-Alad <43155016+Maestro-Alad@users.noreply.github.com>
2019-08-30 01:03:08 -0400

    Add project files.

---
+++ WWA-LAB1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WWA_LAB1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        public ActionResult Semester5Page()
+        {
+            return View();
+        }
+        public ActionResult TestimonialsPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ WWA-LAB1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WWA_LAB1.MvcApplication" Language="C#" %>
